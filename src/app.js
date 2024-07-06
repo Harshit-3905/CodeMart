117787ee876bcd9e7e157b84b9c218995a5371f3
Harshit-3905 <harshitjoshileet@gmail.com>
2024-07-06 18:48:55 +0530

    Add API routes for categories, products, and users

--- src/app.js
+++ src/app.js
@@ -2,4 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+import categoryRouter from "./routers/category.routes.js";
+import productRouter from "./routers/product.routes.js";
+import userRouter from "./routers/user.routes.js";
+
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/user", userRouter);
+
 export default app;
