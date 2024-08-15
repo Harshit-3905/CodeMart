d28830b6f26d5783423abbdfe20140fe7d84952b
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-15 12:19:02 +0530

    Remove unnecessary files and code related to category functionality

--- README.md
+++
@@ -1,1 +0,0 @@
-# CodeMart-Backend
--- src/middlewares/isAdmin.middleware.js
+++
@@ -1,8 +0,0 @@
-import asyncHandler from "../utils/asyncHandler.js";
-
-export const isAdmin = asyncHandler(async (req, res, next) => {
-    if (!req.user.role === "admin") {
-        throw new ApiError(403, "Unauthorized Request");
-    }
-    next();
-});
