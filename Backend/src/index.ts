b92da2c3e3ae0d3bb1e5051fa32fa60cc3c30d8d
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-18 16:44:58 +0530

    Remove unneccessary files

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import userRouter from "./routes/user.route";
 import productRouter from "./routes/product.route";
+import categoryRouter from "./routes/category.route";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(cors());
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/product", productRouter);
+app.route("/api/v1/category", categoryRouter);
 
 app.get("/", (c) => {
   return c.text("Backend Working Fine");
