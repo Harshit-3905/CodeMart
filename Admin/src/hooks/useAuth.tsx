2ce4b2c47c2c1202c809c5984ff8b30a3e17f0e1
Harshit Joshi <harshitjoshileet@gmail.com>
2024-09-07 16:26:34 +0530

    Add authentication management

---
+++ Admin/src/hooks/useAuth.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("adminToken");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8787/api/v1/admin/login",
+        { email, password }
+      );
+      const { token } = response.data;
+      localStorage.setItem("adminToken", token);
+      setIsAuthenticated(true);
+    } catch (err) {
+      throw new Error("Login failed" + err);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("adminToken");
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
