f6bfda6d43c91b740c1bc4db4aaa160c0cff6b95
Harshit Joshi <harshitjoshileet@gmail.com>
2024-09-09 16:26:34 +0530

    Enhance SignUpPage

--- Frontend/src/pages/SignUpPage.tsx
+++ Frontend/src/pages/SignUpPage.tsx
@@ -4,18 +4,23 @@ import { Loader2 } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
 
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-  const SignInHandler = async () => {
+  const { login } = useAuth();
+
+  const SignUpHandler = async () => {
     setLoading(true);
+    setError("");
     try {
-      const { data } = await axios.post(
+      const response = await axios.post(
         "http://localhost:8787/api/v1/user/signup",
         {
           name,
@@ -23,18 +28,33 @@ const SignUpPage = () => {
           password,
         }
       );
-      console.log(data);
-      localStorage.setItem("token", data.token);
-      setLoading(false);
-      navigate("/");
+      if (response.status === 201) {
+        login(response.data.token);
+        navigate("/products");
+      } else {
+        setError(response.data.message || "An error occurred during sign up");
+      }
     } catch (error) {
-      console.log(error);
+      if (axios.isAxiosError(error) && error.response) {
+        if (error.response.status === 409) {
+          setError("User with this email already exists.");
+        } else {
+          setError(
+            error.response.data.message || "An error occurred during sign up"
+          );
+        }
+      } else {
+        setError("An unexpected error occurred");
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-slate-600 text-white">
+    <div className="w-full min-h-[80vh] flex items-center justify-center bg-slate-600 text-white">
       <div className="flex flex-col bg-black p-5 gap-3 rounded-xl">
+        {error && <div className="text-red-500 text-center">{error}</div>}
         <div className="flex flex-col gap-2">
           <Label htmlFor="name">Name</Label>
           <Input
@@ -70,9 +90,9 @@ const SignUpPage = () => {
         </div>
         <div className="flex justify-center pt-4">
           <Button
-            onClick={SignInHandler}
+            onClick={SignUpHandler}
             disabled={loading}
-            className="bg-green-700"
+            className="bg-green-700 w-[300px]"
           >
             {loading ? (
               <Loader2 className="mr-2 h-4 w-4 animate-spin" />
@@ -80,6 +100,12 @@ const SignUpPage = () => {
               "Sign Up"
             )}
           </Button>
+        </div>
+        <div className="flex justify-center">
+          Already have an account ? &nbsp;
+          <Link to="/signin" className="text-red-500">
+            Sign in
+          </Link>
         </div>
       </div>
     </div>
